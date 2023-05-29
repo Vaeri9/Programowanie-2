a0116d40884d4e49add6c7d641ed42a721341887
Vaeri9 <134331378+Vaeri9@users.noreply.github.com>
2023-05-29 23:39:46 +0200

    Update Team.h

--- Zadanie 5/Team.h
+++ Zadanie 5/Team.h
@@ -10,11 +10,9 @@ public:
 	int goals;
 	int lostgoals;
 	int points;
-	//Team();     //jak to jest odkomentowane to wywala potezny error
 	Team(char n, int w, int d, int l, int g, int lg);
 	int countPoints();
 	friend ostream& operator<<(ostream& output, Team& data);
-	//~Team();
 
 
 }; 
