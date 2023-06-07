9c79630dfabf3d5dde0be4d3096116ee7935d92f
Vaeri9 <134331378+Vaeri9@users.noreply.github.com>
2023-06-07 06:38:08 +0200

    Add files via upload

---
+++ Zadanie 6/Student.cpp
@@ -0,0 +1,38 @@
+#include "Student.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+Student::Student(string n, string s, int id, int g, int y) {
+	Name = n;
+	Surname = s;
+	ID = id;
+	Group = g;
+	Year = y;
+};
+
+ifstream& operator>>(ifstream& input, Student& data) {
+	input >> data.Name >> data.Surname >> data.ID >> data.Group >> data.Year;  //tu sie sutomatycznie robi endl
+	return input;
+};
+
+ofstream& operator<<(ofstream& output, Student& data) {
+	output << data.Name << endl << data.Surname << endl << data.ID << endl << data.Group << endl << data.Year << endl;
+	return output;
+};
+
+ostream& operator<<(ostream& output, Student& data) {  //koniecznie te referencje
+	//output << "nazwisko = " << data.nazwisko << ", wiek = " << data.wiek << endl;
+	output << "Name: " << data.Name << " Surname: " << data.Surname << " ID: " << data.ID << " Group: " << data.Group << " Year: " << data.Year << endl;
+	return output;
+};
+
+istream& operator>>(istream& input, Student& data) {
+	cout << "Podaj nazwisko ";
+	input >> data.Surname;   //jak spacja to getline trzeba uzyc
+	cout << "Input year: ";
+	input >> data.Year;
+	return input;
+};
---
+++ Zadanie 6/main.cpp
@@ -0,0 +1,48 @@
+#include "Student.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void fun(Student p) {
+	cout << p;
+};
+
+int main() {
+	vector<Student> Students;
+
+	char n = 65;
+	char s = 97;
+
+	for (int i = 0; i < 9; i++) {
+		string name(1,n);
+		string surname(1, s);
+		int id = (rand() % 101) + 500;
+		int g = 0 + (rand() % 31 + 100);
+		int y = 0 + (rand() % 4 + 1);
+		Students.push_back(Student(name, surname, id, g, y));
+		n++;
+		s = s + 3;
+	};
+
+	ofstream file("data.txt");  
+
+	for_each(Students.begin(), Students.end(), [](Student p) {cout << p << endl; });
+
+	vector<Student> ::iterator p;  
+
+	for (p = Students.begin(); p != Students.end(); p++) {
+		cout << "Zapis studenta " << p->Name << " " << p->Surname << " do pliku" << endl;  
+		file << *p << endl;
+	}
+
+	file.close(); 
+
+
+	cout << endl << count_if(Students.begin(), Students.end(), [](Student p){return (p.Year == 3);  });
+
+
+	return 0;
+}
